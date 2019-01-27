a02ac9fa0bf022ce4565289a04f4c141c9427b3b
bekaweirdo <beqachachua@gmail.com>
2019-01-27 14:24:22 +0100

    add button and recipe-detail into recipe-list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from '../../models/recipes/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+new Recipe('A Test Recipe', 'This is simply a test', 'https://www.mcdonalds.com/content/dam/usa/documents/mcdelivery/mcdelivery_new11.jpg'),
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
