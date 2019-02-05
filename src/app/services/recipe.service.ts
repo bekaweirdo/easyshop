6db7abc4556cba4388f5daf5e3ab88172e63b0ad
bekaweirdo <beqachachua@gmail.com>
2019-02-05 22:00:21 +0100

    call shoppinglistservice method into recipe service

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,16 +1,32 @@
 import {Recipe} from '../models/recipes/recipe.model';
-import {EventEmitter} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from './shopping-list.service';
 
+@Injectable()
 export class RecipeService {
-   public recipeSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test',
-      'https://www.mcdonalds.com/content/dam/usa/documents/mcdelivery/mcdelivery_new11.jpg'),
-    new Recipe(' Another Test Recipe', 'This is simply a test',
-      'https://www.mcdonalds.com/content/dam/usa/documents/mcdelivery/mcdelivery_new11.jpg'),
-  ];
+  public recipeSelected = new EventEmitter<Recipe>();
+  // recipes: Recipe[] = [
+  //   new Recipe('A Test Recipe', 'This is simply a test',
+  //     'https://www.mcdonalds.com/content/dam/usa/documents/mcdelivery/mcdelivery_new11.jpg', [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('French Fries', 20)
+  //     ]),
+  //   new Recipe(' Another Test Recipe', 'This is simply a test',
+  //     'https://www.mcdonalds.com/content/dam/usa/documents/mcdelivery/mcdelivery_new11.jpg', [
+  //       new Ingredient('Bread', 2),
+  //       new Ingredient('Meat', 1)
+  //     ]),
+  // ];
 
-  getRecipes() {
-    return this.recipes.slice();
+  // getRecipes() {
+  //   return this.recipes.slice();
+  // }
+  constructor(private slService: ShoppingListService) {
+
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
