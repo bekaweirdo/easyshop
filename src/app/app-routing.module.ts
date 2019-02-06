e8190c7a59dfce8e76161f390660c255a913236b
bekaweirdo <beqachachua@gmail.com>
2019-02-06 12:58:48 +0100

    add new component into recipe's child component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {RecipesComponent} from './recipes/recipes.component';
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
+import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
 
 const appRoutes: Routes = [
   {
@@ -12,7 +14,10 @@ const appRoutes: Routes = [
   {
     path: 'recipes',
     component: RecipesComponent,
-
+    children: [
+      { path: '' , component: RecipeStartComponent},
+      {path: ':id', component: RecipeDetailComponent}
+    ]
   },
   {
     path: 'shopping-list',
