064ce225fd352bba484d65c67caf7499d5f70cc0
bekaweirdo <beqachachua@gmail.com>
2019-02-06 13:01:23 +0100

    add getRecipe method

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -6,24 +6,29 @@ import {ShoppingListService} from './shopping-list.service';
 @Injectable()
 export class RecipeService {
   public recipeSelected = new EventEmitter<Recipe>();
-  // recipes: Recipe[] = [
-  //   new Recipe('A Test Recipe', 'This is simply a test',
-  //     'https://www.mcdonalds.com/content/dam/usa/documents/mcdelivery/mcdelivery_new11.jpg', [
-  //       new Ingredient('Meat', 1),
-  //       new Ingredient('French Fries', 20)
-  //     ]),
-  //   new Recipe(' Another Test Recipe', 'This is simply a test',
-  //     'https://www.mcdonalds.com/content/dam/usa/documents/mcdelivery/mcdelivery_new11.jpg', [
-  //       new Ingredient('Bread', 2),
-  //       new Ingredient('Meat', 1)
-  //     ]),
-  // ];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test',
+      'https://www.mcdonalds.com/content/dam/usa/documents/mcdelivery/mcdelivery_new11.jpg', [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe(' Another Test Recipe', 'This is simply a test',
+      'https://www.mcdonalds.com/content/dam/usa/documents/mcdelivery/mcdelivery_new11.jpg', [
+        new Ingredient('Bread', 2),
+        new Ingredient('Meat', 1)
+      ]),
+  ];
 
   // getRecipes() {
   //   return this.recipes.slice();
   // }
   constructor(private slService: ShoppingListService) {
 
+  }
+
+  getRecipe(id: number) {
+    // slice = copy of array
+    return this.recipes.slice()[id];
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
