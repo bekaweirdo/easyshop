ccd41cbb8a086a2a08481afaca5a022c5036071a
bekaweirdo <beqachachua@gmail.com>
2019-02-20 22:25:30 +0100

    add onSave and onFetch method for save and fetch information

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,22 @@
 import {Component} from '@angular/core';
+import {DataStorageService} from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  constructor(private dataStorageService: DataStorageService) {
+  }
 
+  onSaveData() {
+    this.dataStorageService.storeRecipes()
+      .subscribe((response: Response) => {
+        console.log(response);
+      });
+  }
+
+  onFetchData() {
+    this.dataStorageService.getRecipes();
+  }
 }
