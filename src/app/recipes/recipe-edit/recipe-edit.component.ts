68b817aba40ee04353e136632e0a537a48b5efdb
bekaweirdo <beqachachua@gmail.com>
2019-02-20 22:25:56 +0100

    add method onDeleteIngredient into recipe-edit

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -84,4 +84,8 @@ export class RecipeEditComponent implements OnInit {
       })
     );
   }
+
+  onDeleteIngredient(index: number) {
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
 }
