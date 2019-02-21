445dabedd2f49200c60791fe2be50368fd397b6b
bekaweirdo <beqachachua@gmail.com>
2019-02-21 13:24:56 +0100

    add signup component into ap-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
 import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
 import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
+import {SignupComponent} from './auth/signup/signup.component';
 
 const appRoutes: Routes = [
   {
@@ -16,12 +17,13 @@ const appRoutes: Routes = [
     path: 'recipes',
     component: RecipesComponent,
     children: [
-      { path: '' , component: RecipeStartComponent},
-      { path: 'new', component: RecipeEditComponent},
-      { path: ':id', component: RecipeDetailComponent},
-      { path: ':id/edit', component: RecipeEditComponent}
+      {path: '', component: RecipeStartComponent},
+      {path: 'new', component: RecipeEditComponent},
+      {path: ':id', component: RecipeDetailComponent},
+      {path: ':id/edit', component: RecipeEditComponent},
     ]
   },
+  {path: 'signup', component: SignupComponent},
   {
     path: 'shopping-list',
     component: ShoppingListComponent,
