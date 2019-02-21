1086453d8ad366fc226307af839df8d3faa5a11b
bekaweirdo <beqachachua@gmail.com>
2019-02-21 23:48:53 +0100

    add initializeApp for firebase into app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
+  }
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyB4zxMcUZZZN1g6Ab0DFQdau0wpnr5A2N8',
+      authDomain: 'easyshop-4af87.firebaseapp.com',
+    });
   }
 }
