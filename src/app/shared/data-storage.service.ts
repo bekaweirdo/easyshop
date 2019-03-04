dda1b5642b586c5e9c5892bc9ea6ea37f3d39d02
bekaweirdo <beqachachua@gmail.com>
2019-03-04 12:37:21 +0100

    add options for responseType and observe

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -19,9 +19,10 @@ export class DataStorageService {
 
   getRecipes() {
     const token = this.authservice.getToken();
-    return this.http.get('https://easyshop-4af87.firebaseio.com/recipes.json?auth=' + token)
+    // return this.http.get<Recipe[]>('https://easyshop-4af87.firebaseio.com/recipes.json?auth=' + token)
+    return this.http.get('https://easyshop-4af87.firebaseio.com/recipes.json?auth=' + token, {observe: 'body', responseType: 'json'})
       .subscribe(
-        (response: Response) => {
+        (response) => {
           // TODO: Error in type
           const recipes: any = response;
           for (const recipe of recipes) {
