6173bfb774f77277081bc2f202d9f6ef2e7f6bc1
bekaweirdo <beqachachua@gmail.com>
2019-03-04 18:00:06 +0100

    override Action into shoppingListReducer and import state

--- src/app/shopping-list/store/shopping-list.reducers.ts
+++ src/app/shopping-list/store/shopping-list.reducers.ts
@@ -1,4 +1,4 @@
-import {Action} from '@ngrx/store';
+import * as ShoppingListActions from 'shopping-list.actions';
 import {Ingredient} from '../../shared/ingredient.model';
 
 
@@ -9,13 +9,14 @@ const initialState = {
   ]
 };
 
-export function shoppingListReducer(state = initialState, action: Action) {
+export function shoppingListReducer(state = initialState, action: ShoppingListActions.ShoppingListActions) {
   switch (action.type) {
-    case ADD_INGREDIENT:
-      return{
+    case ShoppingListActions.ADD_INGREDIENT:
+      return {
         ...state,
-        ingredients: [ ...state.ingredients, action]
-      }
+        ingredients: [...state.ingredients, action.payload]
+      };
+    default:
+      return state;
   }
-  return state;
 }
